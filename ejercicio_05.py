7314eafae849e6d366d0c7ba503f7d31c652be43
Wicho1255 <zoneksitohc@gmail.com>
2026-01-23 10:36:07 -0600

    Mi segundo commit

---
+++ ejercicio_05.py
@@ -0,0 +1,6 @@
+# Convertir en grados fahrenheit a grados Clesuis
+
+fahrenheit = float(input('Ingresa los grados fahrenheit:'))
+Celsius = (fahrenheit - 32) * (5 / 9)
+print('fahrenheit', fahrenheit)
+print('Celsius', Celsius)
--- variables.py
+++ variables.py
@@ -2,6 +2,9 @@
 #Python es un lenguaje no tipado
 #Detecta el tipo de dato dependiendo del valor
 #Comienza con _ a-z A-Z 0-9
+#Para python utilizamos snake case
+#Número de matricula numero_de_matricula
+#Número de empleado numero_de_empleado
 
 name = "Wicho"
 age = 22
