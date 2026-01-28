712e9241d20746d2385f0ee6192bc063c74e87b8
Wicho1255 <zoneksitohc@gmail.com>
2026-01-28 16:03:59 -0600

    Ejercicio 06

---
+++ condicionales/ejercicio_06.py
@@ -0,0 +1,9 @@
+#programa que lee una letra y dice si es mayuscula 
+
+letter = input('Ingresa una letra: ')
+
+if letter == letter.upper():
+	print('Es una letra mayuscula')
+else:
+	print('No es una letra mayuscula')
+	
